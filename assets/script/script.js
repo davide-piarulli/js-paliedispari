67c59aaf44bcd32e5d2780684d9f4a1b155ba349
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-28 14:51:16 +0000

    palindroma

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,10 @@
 let parola = prompt('Scrivi una parola palindroma');
-console.log(parola);
-
 let parolaDivisa = parola.split('');
-console.log(parolaDivisa);
+let parolaReverse = parolaDivisa.reverse();
+let parolaInvertita = parolaReverse.join('');
+
+if(parola == parolaInvertita){
+  document.getElementById('outputpali').innerHTML = `Ottimo la parola: <strong>${parola}</strong> è PALINDROMA`;
+} else {
+  document.getElementById('outputpali').innerHTML = `Hai sbagliato, la parola: <strong>${parola}</strong> non è palindroma`;
+}
