e0b9e1af3974fe209fb2886d79dc04f6f1901784
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-28 15:12:22 +0000

    tentativo con button, NON funziona

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,20 @@
-let parola = prompt('Scrivi una parola palindroma');
 let parolaDivisa = parola.split('');
 let parolaReverse = parolaDivisa.reverse();
 let parolaInvertita = parolaReverse.join('');
+const btn1 = document.getElementById('btn1');
 
-if(parola == parolaInvertita){
-  document.getElementById('outputpali').innerHTML = `Ottimo la parola: <strong>${parola}</strong> è PALINDROMA`;
-} else {
-  document.getElementById('outputpali').innerHTML = `Hai sbagliato, la parola: <strong>${parola}</strong> non è palindroma`;
-}
+
+btn1.addEventListener('click', function(){
+  let parola = prompt('Scrivi una parola palindroma');
+
+  if(parola == parolaInvertita){
+    document.getElementById('outputpali').innerHTML = `Ottimo la parola: <strong>${parola}</strong> è PALINDROMA`;
+  } else {
+    document.getElementById('outputpali').innerHTML = `Hai sbagliato, la parola: <strong>${parola}</strong> non è palindroma`;
+  }
+})
+
+
+
+
+
