c7a80ea13be957d3360f9db7344d7a4e30872be4
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-28 16:49:30 +0000

    fix esercizio palindromo

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,54 +1,78 @@
 // Palindroma
-
 const btn1 = document.getElementById('btnpali');
 
-
+function parolaPalindroma(stringa) {
+  // divido la parola
+  for (let i = 0; i < stringa.length / 2; i++) {
+    if (stringa[i] !== stringa[stringa.length - 1 - i]) {
+      return false;
+    } 
+  }
+  return true;
+}
 btn1.addEventListener('click', function(){
   let parola = prompt('Scrivi una parola palindroma');
-  let parolaDivisa = parola.split('');
-  let parolaReverse = parolaDivisa.reverse();
-  let parolaInvertita = parolaReverse.join('');
-
-  if(parola == parolaInvertita){
-    document.getElementById('outputpali').innerHTML = `Ottimo la parola: <strong>${parola}</strong> è PALINDROMA`;
+if (parola !== null && parola === parola.toLowerCase()) {
+  if (parolaPalindroma(parola)) {
+    document.getElementById('outputpali').innerHTML = 
+    `Ottimo la parola: <strong>${parola}</strong> è PALINDROMA`;
   } else {
-    document.getElementById('outputpali').innerHTML = `Hai sbagliato, la parola: <strong>${parola}</strong> non è palindroma`;
-  }
+    document.getElementById('outputpali').innerHTML = 
+    `Hai sbagliato, la parola: <strong>${parola}</strong> non è palindroma`;
+    }
+}
 })
 
 
 // Pari e Dispari
-let pariDispari;
-let numGiocatore;
+const btn2 = document.getElementById('btnparidisp');
 
-let validPariDispari = false;
-let validNumber = false;
+btn2.addEventListener('click', function(){
+  let pariDispari;
+  let numGiocatore;
+  let validPariDispari = false;
+  let validNumber = false;
+  const numPc = numRandom();
 
-// chiedo di inserire pari o dispari e verifico se è scritto bene
-while (!validPariDispari) {
-  pariDispari = prompt('Scrivi "pari" o "dispari"');
-
-  if(pariDispari.toLowerCase() === 'pari' || pariDispari.toLowerCase() === 'dispari'){
-    validPariDispari = true;
-  }else{
-    alert('NON hai scritto pari o dispari!');
-  }
-}
-
-while (!validNumber) {
-  numGiocatore = parseInt(prompt('Scrivi un numero da 1 a 5'));
-  if(isNaN(numGiocatore) === false){
-    if(numGiocatore > 0 && numGiocatore <=5){
-      validNumber = true;
+  // chiedo di inserire pari o dispari e verifico se è scritto bene
+  while (!validPariDispari) {
+    pariDispari = prompt('Scrivi "pari" o "dispari"');
+  
+    if(pariDispari.toLowerCase() === 'pari' || pariDispari.toLowerCase() === 'dispari'){
+      validPariDispari = true;
     }else{
-      alert('Hai inserito un numero errato');
+      alert('NON hai scritto pari o dispari!');
     }
-  }else{
-    alert('Hai inserito una stringa invece che un numero');
   }
-}
-document.getElementById('outputparidisp').innerHTML = 
-`
-Hai scelto: ${pariDispari} <br>
-Il tuo numero è: ${numGiocatore}
-`;
+  
+  while (!validNumber) {
+    numGiocatore = parseInt(prompt('Scrivi un numero da 1 a 5'));
+    if(isNaN(numGiocatore) === false){
+      if(numGiocatore > 0 && numGiocatore <=5){
+        validNumber = true;
+      }else{
+        alert('Hai inserito un numero errato');
+      }
+    }else{
+      alert('Hai inserito una stringa invece che un numero');
+    }
+  }
+  document.getElementById('outputparidisp').innerHTML = 
+  `
+  Hai scelto: ${pariDispari} <br>
+  Il tuo numero è: ${numGiocatore} <br>
+  Il numero random del PC è: ${numPc} <br>
+  `;
+
+  // funzione per il numero casuale del PC
+  function numRandom() {
+    // Genera un numero casuale compreso tra 1 e 5
+    return Math.floor(Math.random() * 5) + 1;
+  }
+
+  // function somma ()
+
+
+
+
+})
